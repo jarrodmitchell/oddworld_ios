3847f28fff1a4ab6f4be39ce7b56c0c29edcb2bb
jc <61209332+penguindrummmer@users.noreply.github.com>
2020-09-06 10:15:41 -0400

    first commit

---
+++ High Strangeness/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  High Strangeness
+//
+//  Created by penguindrum on 9/6/20.
+//  Copyright © 2020 penguindrum. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ High Strangeness/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  High Strangeness
+//
+//  Created by penguindrum on 9/6/20.
+//  Copyright © 2020 penguindrum. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
